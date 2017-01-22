33c5f7feaa40e4a13d86055044f4d5fe9ff0816c
Thanh Nguyen <thanhnguyen@Thanhs-MBP.net.fpt>
2017-01-23 00:14:05 +0700

    Birthday Problem Assignment

---
+++ app/src/main/java/mooc/vandy/java4android/birthdayprob/logic/Logic.java
@@ -0,0 +1,104 @@
+package mooc.vandy.java4android.birthdayprob.logic;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import mooc.vandy.java4android.birthdayprob.ui.OutputInterface;
+
+/**
+ * This is where the logic of this App is centralized for this assignment.
+ * <p>
+ * The assignments are designed this way to simplify your early Android interactions.
+ * Designing the assignments this way allows you to first learn key 'Java' features without
+ * having to beforehand learn the complexities of Android.
+ *
+ */
+public class Logic 
+       implements LogicInterface {
+    /**
+     * This is a String to be used in Logging (if/when you decide you
+     * need it for debugging).
+     */
+    public static final String TAG =
+        Logic.class.getName();
+
+    /**
+     * This is the variable that stores our OutputInterface instance.
+     * <p>
+     * This is how we will interact with the User Interface
+     * [MainActivity.java].
+     * <p>
+     * It is called 'mOut' because it is where we 'out-put' our
+     * results. (It is also the 'in-put' from where we get values
+     * from, but it only needs 1 name, and 'mOut' is good enough).
+    */
+    OutputInterface mOut;
+
+    /**
+     * This is the constructor of this class.
+     * <p>
+     * It assigns the passed in [MainActivity] instance
+     * (which implements [OutputInterface]) to 'out'
+     */
+    public Logic(OutputInterface out){
+        mOut = out;
+    }
+
+    /**
+     * This is the method that will (eventually) get called when the
+     * on-screen button labelled 'Process...' is pressed.
+     */
+    public void process() {
+        int groupSize = mOut.getSize();
+        int simulationCount = mOut.getCount();
+
+        if (groupSize < 2 || groupSize > 365) {
+            mOut.makeAlertToast("Group Size must be in the range 2-365.");
+            return;
+        }
+        if (simulationCount <= 0) {
+            mOut.makeAlertToast("Simulation Count must be positive.");
+            return;
+        }
+
+        double percent = calculate(groupSize, simulationCount);
+
+        // report results
+        mOut.println("For a group of " + groupSize + " people, the percentage");
+        mOut.println("of times that two people share the same birthday is");
+        mOut.println(String.format("%.2f%% of the time.", percent));
+
+    }
+
+    /**
+     * This is the method that actually does the calculations.
+     * <p>
+     * We provide you this method that way we can test it with unit testing.
+     */
+    private static final int daysInYear = 365;
+    private static final Random random = new Random();
+
+    public double calculate(int groupSize, int simulationCount) {
+        // TODO -- add your code here
+        int numberOfMatches = 0;
+
+        for (int i = 0; i < simulationCount; ++i) {
+            if (isMatchInTrial(groupSize)) {
+                numberOfMatches++;
+            }
+        }
+        double percent = (100.00 * numberOfMatches / simulationCount);
+        return percent;
+    }
+
+    // TODO - add your code here
+    private static boolean isMatchInTrial(int groupSize) {
+        Set<Integer> birthdays = new HashSet<>();
+        for (int i = 0; i < groupSize; ++i) {
+            birthdays.add(random.nextInt(daysInYear));
+        }
+        return birthdays.size() != groupSize;
+    }
+
+}
